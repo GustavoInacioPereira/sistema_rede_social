35b649bacd79a086a826b1e16ab3e412b5654ca9
GustavoInacioPereira <gustavo0207yt@gmail.com>
2026-03-10 21:41:58 -0300

    fix: alteracao caminho relativo dos documentos, tendo como basse a pasta main

--- main/src/paths/PathDocuments.java
+++ main/src/paths/PathDocuments.java
@@ -1,5 +1,5 @@
 package paths;
 public class PathDocuments {
-    public static final String USER_PATH = "src/documents/UserInfos.txt";
-    public static final String POSTS_PATH = "src/documents/PostsInfos.txt";
+    public static final String USER_PATH = "main/src/documents/UserInfos.txt";
+    public static final String POSTS_PATH = "main/src/documents/PostsInfos.txt";
 }
